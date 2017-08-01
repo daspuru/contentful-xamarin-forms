46198e67e483eb9bd5774dcc9f6c85d56c0b76ce
daspuru <aspuru@arsophia.com>
2017-08-01 11:40:09 -0500

    Initial push.

---
+++ ContentfulFormsTest/ContentfulFormsTestPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Contentful.Core;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Contentful.Core.Search;
+using Contentful.Core.Models;
+
+namespace ContentfulFormsTest
+{
+    public partial class ContentfulFormsTestPage : ContentPage
+    {
+        public ContentfulFormsTestPage()
+        {
+            InitializeComponent();
+            getLabel();
+        }
+
+        public async Task getLabel() {
+            var httpClient = new HttpClient();
+            var client = new ContentfulClient(httpClient, "Your Contentful's Key", "Your Contentful's Space Id");
+            var entry = await client.GetEntryAsync<Entry<dynamic>>("Your Entry's Id");
+            textLabel.Text = entry.Fields.encabezado.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # contentful-xamarin-forms
 This is a test for using Contentful.Net in a Xamarin.Forms project. It loads a string of content in the main page.
+
+Just edit and put your Contentful's Key, Space Id and Content Id in ContentfulFormsTestPage.xaml.cs and the solution will load the string content into the view at load.
+
+Tested in iOS and Android.
+
